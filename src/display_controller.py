6bd9b04a3a380c5d4f724ea470fc8fd2595b6488
ChuckBuilds <33324927+ChuckBuilds@users.noreply.github.com>
2025-04-17 09:48:53 -0500

    Force DEBUG logging level in display_controller

--- src/display_controller.py
+++ src/display_controller.py
@@ -9,7 +9,7 @@ from src.stock_manager import StockManager
 from src.stock_news_manager import StockNewsManager
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, force=True)
 logger = logging.getLogger(__name__)
 
 class DisplayController:
