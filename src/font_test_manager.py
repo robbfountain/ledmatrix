3f656d60de8bee65a00a1a4a62d7c54dcb0a6cdf
ChuckBuilds <33324927+ChuckBuilds@users.noreply.github.com>
2025-04-22 19:34:01 -0500

    change font sizing

--- src/font_test_manager.py
+++ src/font_test_manager.py
@@ -19,7 +19,7 @@ class FontTestManager:
             "4x6": "assets/fonts/4x6-font.ttf",
             "MatrixChunky6": "assets/fonts/MatrixChunky6.ttf"
         }
-        self.font_sizes = [6, 8, 10, 12, 14, 16, 18, 20]
+        self.font_sizes = [6]
         self.logger = logging.getLogger('FontTest')
         
         # Verify fonts exist
