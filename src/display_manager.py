9dedfe02640a616df2b3e866b0394f11ad39fda5
Chuck <33324927+ChuckBuilds@users.noreply.github.com>
2025-07-23 16:18:07 -0500

    shift bdf draw  text down one mor epixel total 6

--- src/display_manager.py
+++ src/display_manager.py
@@ -146,8 +146,8 @@ class DisplayManager:
             # Use the passed font or fall back to calendar_font
             face = font if font else self.calendar_font
             
-            # Add 5-pixel adjustment for vertical alignment
-            y += 5
+            # Add 6-pixel adjustment for vertical alignment
+            y += 6
             
             for char in text:
                 face.load_char(char)
